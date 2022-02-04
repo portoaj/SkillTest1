712973dfff2a0fed39de0a83e3abbb6c8e9863bf
Andrew Masek <andrewmasek@gmail.com>
2022-02-04 15:31:12 -0800

    initial commit

---
+++ Example.java
@@ -0,0 +1,5 @@
+public class Example {
+    public static boolean returnTrue() {
+        return false;
+    }
+}
---
+++ ExampleTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void testExample() {
+        assertTrue(Example.returnTrue());
+    }
+}
